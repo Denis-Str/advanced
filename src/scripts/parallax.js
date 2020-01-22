7739df66b3df3103eb41ecaeb8c5d1193ac5214f
Denis <web_life@inbox.ru>
2020-01-22 21:54:16 +0300

    добавил паралакс на сайт

---
+++ src/scripts/parallax.js
@@ -0,0 +1,47 @@
+const parallaxPromo = document.querySelector('.promo__parallax');
+const layersPromo = parallaxPromo.children;
+
+const parallaxBudda = document.querySelector('.reviews__parallax');
+const layersBudda = parallaxBudda.children;
+
+const moveLayers = (wScroll, layers) => {
+    Array.from(layers).forEach(layer => {
+        const dataSet = layer.dataset.speed;
+        const scroll = dataSet / 60 * wScroll;
+
+        layer.style.transform = `translate3d(0, ${-scroll}%, 0)`
+    })
+};
+
+// function moveLayers(wScroll) {
+//
+//     Array.from(layers).forEach(layer => {
+//         const dataSet = layer.dataset.speed;
+//         const scroll = dataSet / 50 * wScroll;
+//
+//         layer.style.transform = `translate3d(0, ${-scroll}%, 0)`
+//
+//     })
+// }
+// let parallax = (function () {
+//
+//     return {
+//         move: function (section, wScroll, ratio) {
+//             let scroll = wScroll / -ratio + '%';
+//             let transformSection = `translate3d(0, ${scroll}, 0)`;
+//             let style = section.style;
+//             style.transform = transformSection;
+//             style.webkitTransform = transformSection;
+//         },
+//         init: function (wScroll) {
+//             this.move(sky, wScroll, 45);
+//
+//         }
+//     }
+// })();
+
+window.onscroll = function () {
+    let wScroll = window.pageYOffset;
+    moveLayers(wScroll, layersPromo);
+    moveLayers(wScroll, layersBudda);
+};
--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,27 @@
-console.log('this is skills module');
+let svgScroll = (function () {
+    let svg = document.querySelector('.stack__svg '),
+        // svgPath = document.querySelector('.stack__svg stack__circle-upper'),
+        windowMargin = window.innerHeight / 3,
+        svgRect = svg.getBoundingClientRect(),
+        svgPos = svgRect.top;
+
+    return {
+        grow: function (wScroll) {
+            let startAnim = wScroll - svgPos + windowMargin,
+                pixelsElapsed = svgPos - wScroll,
+                percentsElapsed = 100 - Math.ceil(pixelsElapsed / windowMargin * 100),
+                percentsDraw = 252 / 100 * percentsElapsed;
+
+            if (startAnim >=0)  {
+                let DrawAmount = svg.strokeDashoffset - percentsDraw;
+            }
+
+            // console.log(svg)
+        }
+    }
+})();
+
+window.onscroll = function () {
+    let wScroll = window.pageYOffset;
+    svgScroll.grow(wScroll);
+};

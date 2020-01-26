da4c97178e4d6d5e96cfa944d1514a2c478bf967
Denis <web_life@inbox.ru>
2020-01-26 18:35:29 +0300

    подключаем компоненты :)

---
+++ src/scripts/reviewsSlider.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+
+new Vue({
+   el: '#reviews-component',
+   template: '#reviews',
+   components: {
+      swiper,
+      swiperSlide
+   }
+});
--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -25,9 +25,48 @@
 //     svgScroll.grow(wScroll);
 // };
 
-// new Vue({
-//     template: '<p>message</p>',
-//     data: {
-//
-//         message: 'Привет, Vue!'
-// }});
+import Vue from 'vue';
+
+
+const skill = {
+    template: '#skill',
+    props: ['skillName', 'skillPercent'],
+    methods: {
+      drawColorCircle() {
+          const circle = this.$refs["color-circle"];
+          const dashArray = parseInt(
+              getComputedStyle(circle).getPropertyValue('stroke-dasharray')
+          );
+          const percent = (dashArray / 100) * (100 - this.skillPercent);
+          circle.style.strokeDashoffset = percent;
+      }
+    },
+    mounted() {
+        this.drawColorCircle()
+    }
+};
+
+const skillsRow = {
+    template: '#skills-row',
+    components: {
+        skill
+    },
+    props: ['skill']
+};
+
+new Vue({
+    el: '#skills-block',
+    template: '#stack',
+    data() {
+        return {
+            skills: []
+        }
+    },
+    components: {
+        skillsRow
+    },
+    created() {
+        const data = require('../data/skills.json');
+        this.skills = data;
+    }
+});
